7dc81a099267477e71a7d2e48597351d709325a4
Alex Ghiculescu <alexghiculescu@gmail.com>
2013-09-26 16:03:37 +1000

    tweaks

--- mandrill.rb
+++ mandrill.rb
@@ -5,6 +5,10 @@ class Mandrill
   def self.process(events, color)
     JSON.parse(events).each do |event|
       msg = event['msg']
+      next if msg['email'].index("@payaus.com").present? # don't track if we open it
+      next if msg['tags'].empty? # only display tagged emails
+      next if msg['opens'].length > 1 # don't repeatedly show an email
+
       message = "Email #{color == HipChat::Colors::EMAIL_OPEN ? 'opened' : 'clicked'} by #{msg['email']}. Subject: #{msg['subject']}."
       unless msg['url'].nil?
         message = "#{message} URL: #{msg['url']}"
