65259a78256dacf06f64e1a9a0f712778800fd7f
Alex Ghiculescu <alexghiculescu@gmail.com>
2014-01-06 11:15:33 +1000

    ignore @tanda.co

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bundle exec puma -e production -p 7263 -S ~/puma -t 1:2 -C config/puma.rb
+web: bundle exec puma -e production -p 7263 -S ~/puma -t 1:1
--- mandrill.rb
+++ mandrill.rb
@@ -4,11 +4,10 @@ require 'json'
 class Mandrill
   def self.process(events, color, notify = false)
     JSON.parse(events).each do |event|
-      puts
-      puts event
       msg = event['msg']
       next if msg['email'].nil? || msg['subject'].nil?
       next unless msg['email'].index("@payaus.com").nil? # don't track if we open it
+      next unless msg['email'].index("@tanda.co").nil? # don't track if we open it
       next if msg['tags'].empty? # only display tagged emails
 
       message = "Email #{color == HipChat::Colors::EMAIL_OPEN ? 'opened' : 'clicked'} by #{msg['email']}. Subject: #{msg['subject']}."
